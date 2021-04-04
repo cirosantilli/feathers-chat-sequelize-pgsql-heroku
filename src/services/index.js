0e17738676d1f784e30a33a9007acca1886cd31c
Ciro Santilli <ciro.santilli@gmail.com>
2021-04-04 12:25:29 +0100

    remove useless db thing

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,7 @@
 const users = require('./users');
 const messages = require('./messages/messages.service.js');
-const db = require('./db/db.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(messages);
-  app.configure(db);
 };
